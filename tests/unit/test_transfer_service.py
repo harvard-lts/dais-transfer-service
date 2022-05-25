069bbcf155608db37ab173594df8e75ee5be0da8
Valdeva Crema <valdeva_crema@harvard.edu>
2022-05-25 16:53:08 -0400

    Allow for empty data directory which requires no hash mapping file (#11)

--- tests/unit/test_transfer_service.py
+++ tests/unit/test_transfer_service.py
@@ -3,16 +3,17 @@ sys.path.append('app')
 import transfer_service.transfer_validation as transfer_validation 
 import transfer_service.transfer_service as transfer_service 
 
-s3_path="doi-12-3456-transfer-service-test"
+doi_path="doi-12-3456-transfer-service-test"
+doi_no_data_path="doi-12-3456-transfer-service-test-no-data"
 data_path = "/home/appuser/tests/data"
 
-def test_unzip():
-    dest_path = os.path.join(data_path, s3_path)
+def test_unzip_with_data():
+    dest_path = os.path.join(data_path, doi_path)
     zipextractionpath = transfer_service.unzip_transfer(dest_path)
     #Verify that the extracted directory now exists
     assert os.path.exists(zipextractionpath)
 
-    cleanup_extraction(os.path.join(data_path, s3_path, "extracted"))
+    cleanup_extraction(os.path.join(data_path, doi_path, "extracted"))
 
 def cleanup_extraction(zipextractionpath):
     '''Removes the data that was downloaded into the dropbox'''
@@ -21,8 +22,8 @@ def cleanup_extraction(zipextractionpath):
     except OSError as e:
         print("Error: %s : %s" % (zipextractionpath, e.strerror))
         
-def test_validate():
-    dest_path = os.path.join(data_path, s3_path)
+def test_validate_with_data():
+    dest_path = os.path.join(data_path, doi_path)
     zipextractionpath = transfer_service.unzip_transfer(dest_path)
     #Verify that the extracted directory now exists
     #Verify that the hash mapping file exists.
@@ -31,6 +32,17 @@ def test_validate():
     #Verify that the hash mapping file exists.
     assert os.path.exists(os.path.join(zipextractionpath, os.getenv("SUPPLIED_HASH_MAPPING_FILENAME")))
     
-    assert transfer_validation.validate_transfer(zipextractionpath, os.path.join(data_path, s3_path))
+    assert transfer_validation.validate_transfer(zipextractionpath, os.path.join(data_path, doi_path))
+
+    cleanup_extraction(os.path.join(data_path, doi_path, "extracted"))
     
-    cleanup_extraction(os.path.join(data_path, s3_path, "extracted"))
+def test_validate_with_no_data():
+    dest_path = os.path.join(data_path, doi_no_data_path)
+    zipextractionpath = transfer_service.unzip_transfer(dest_path)
+    #Verify that the extracted directory now exists
+    #Verify that the hash mapping file exists.
+    assert os.path.exists(zipextractionpath)
+    
+    assert transfer_validation.validate_transfer(zipextractionpath, os.path.join(data_path, doi_no_data_path))
+    
+    cleanup_extraction(os.path.join(data_path, doi_no_data_path, "extracted"))
