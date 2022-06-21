84038fbc018a89fa2958306451439a1fe391c17d
Guillermo Portas <hey@gportas.me>
2022-06-21 17:44:07 +0100

    Fix duplicated consumers and connectivity issues (#14)
    * Changed: subscribing connection right after creation
    
    * Stash: listener redesign WIP
    
    * Changed: listener redesign with tests updated
    
    * Added: message acks for queue cleanup in tests
    
    * Fixed: mqutils import in test
    
    * Fixed: test_mqlistener to use ordered dicts

--- tests/integration/test_mqlistener.py
+++ tests/integration/test_mqlistener.py
@@ -1,142 +1,145 @@
-import sys, os, os.path, logging, time, json
+import json
+import logging
+import os
+import os.path
+import sys
+import time
+from collections import OrderedDict
+from unittest.mock import patch
+
 sys.path.append('app/mqresources')
 sys.path.append('app/transfer_service')
 import mqutils
-import mqlistener
+from listener.transfer_ready_queue_listener import TransferReadyQueueListener
 import transfer_helper
 
 logging.basicConfig(format='%(message)s')
 
 transfer_queue = "/queue/transfer-ready-testing-1"
 transfer_queue2 = "/queue/transfer-ready-testing-2"
-s3_bucket=os.getenv("S3_BUCKET_NAME", "dataverse-export-dev")
-s3_path="doi-12-3456-transfer-service-test"
-destination_path=os.path.join("/home/appuser/local/dropbox",s3_path)
+s3_bucket = os.getenv("S3_BUCKET_NAME", "dataverse-export-dev")
+s3_path = "doi-12-3456-transfer-service-test"
+destination_path = os.path.join("/home/appuser/local/dropbox", s3_path)
 
-def test_listener():
+
+@patch("listener.transfer_ready_queue_listener.TransferReadyQueueListener._handle_received_message")
+@patch("listener.transfer_ready_queue_listener.TransferReadyQueueListener._get_queue_name")
+def test_listener(get_queue_name_mock, handle_received_message_mock):
     '''Tests to see if the listener picks up a message from the queue'''
-    mqlistenerobject = mqlistener.get_mqlistener(transfer_queue)
-    
-    conn = mqlistenerobject.get_connection()
-    conn.set_listener('', mqlistenerobject)
-    mqlistener.subscribe_to_listener(mqlistenerobject.connection_params)
-    
-    #This call only puts a dry run message on the queue so it will not validate and transfer
-    message = notify_dry_run_data_ready_transfer_message()
-    messagedict = json.loads(message)
-    
+    get_queue_name_mock.return_value = transfer_queue
+    mq_listener_object = TransferReadyQueueListener()
+
+    # This call only puts a dry run message on the queue so it will not validate and transfer
+    message_json = notify_dry_run_data_ready_transfer_message()
+
     counter = 0
-    #Try for 30 seconds then fail
-    while mqlistenerobject.get_message_data() is None:
+    # Try for 30 seconds then fail
+    while not handle_received_message_mock.call_count:
         time.sleep(2)
-        counter = counter+2
-        if not conn.is_connected():
-            mqlistener.subscribe_to_listener(mqlistenerobject.connection_params)
+        counter = counter + 2
         if counter >= 10:
             assert False, "test_listener: could not find anything on the {} after 30 seconds".format(transfer_queue)
-         
-    assert mqlistenerobject.get_message_data() is not None
-    assert type(mqlistenerobject.get_message_data()) is dict
-    assert mqlistenerobject.get_message_data() == messagedict
-    
-    mqlistenerobject.connection_params.conn.unsubscribe(1)
-   
-def test_listener_and_transfer():
-    '''Tests to see if the listener picks up a message from the queue and triggers the transfer'''
-    #Upload the data to s3 to test the dropbox transfer
-    transfer_helper.upload_sample_data(s3_bucket, s3_path)
-    
-    mqlistenerobject = mqlistener.get_mqlistener(transfer_queue2)
-    
-    conn = mqlistenerobject.get_connection()
-    conn.set_listener('', mqlistenerobject)
-    mqlistener.subscribe_to_listener(mqlistenerobject.connection_params)
-    
-    #This call puts a real message on the queue so it should also do the transfer.
-    message = notify_data_ready_transfer_message()
-    messagedict = json.loads(message)
-    
-    counter = 0
-    #Try for 30 seconds then fail
-    while mqlistenerobject.get_message_data() is None:
-        time.sleep(2)
-        counter = counter+2
-        if not conn.is_connected():
-            mqlistener.subscribe_to_listener(mqlistenerobject.connection_params)
-        if counter >= 10:
-            assert False, "test_listener: could not find anything on the {} after 30 seconds".format(transfer_queue2)
-         
-    assert mqlistenerobject.get_message_data() is not None
-    assert type(mqlistenerobject.get_message_data()) is dict
-    assert mqlistenerobject.get_message_data() == messagedict 
-    assert os.path.exists(destination_path)
-    
-    #cleanup the data that was moved to the dropbox
-    transfer_helper.cleanup_dropbox(destination_path)
-    mqlistenerobject.connection_params.conn.unsubscribe(1)
-    
 
-       
+    args, kwargs = handle_received_message_mock.call_args
+    assert type(args[0]) is dict
+    assert OrderedDict(args[0]) == OrderedDict(message_json)
+
+    # cleanup the queue and disconnect the listener
+    mq_listener_object._acknowledge_message(args[1], args[2])
+    mq_listener_object.disconnect()
+
+
+@patch("listener.transfer_ready_queue_listener.TransferReadyQueueListener._handle_received_message")
+@patch("listener.transfer_ready_queue_listener.TransferReadyQueueListener._get_queue_name")
+def test_listener_and_transfer(get_queue_name_mock, handle_received_message_mock):
+    '''Tests to see if the listener picks up a message from the queue and triggers the transfer'''
+    # Upload the data to s3 to test the dropbox transfer
+    transfer_helper.upload_sample_data(s3_bucket, s3_path)
+
+    get_queue_name_mock.return_value = transfer_queue2
+    mq_listener_object = TransferReadyQueueListener()
+
+    # This call puts a real message on the queue so it should also do the transfer.
+    message_json = notify_data_ready_transfer_message()
+
+    counter = 0
+    # Try for 30 seconds then fail
+    while not handle_received_message_mock.call_count:
+        time.sleep(2)
+        counter = counter + 2
+        if counter >= 10:
+            assert False, "test_listener: could not find anything on the {} after 30 seconds".format(transfer_queue)
+
+    args, kwargs = handle_received_message_mock.call_args
+    assert type(args[0]) is dict
+    assert OrderedDict(args[0]) == OrderedDict(message_json)
+    assert os.path.exists(destination_path)
+
+    # cleanup the data that was moved to the dropbox
+    transfer_helper.cleanup_dropbox(destination_path)
+    # cleanup the queue and disconnect the listener
+    mq_listener_object._acknowledge_message(args[1], args[2])
+    mq_listener_object.disconnect()
+
+
 def notify_dry_run_data_ready_transfer_message():
     '''Creates a dummy queue json message to notify the queue that the 
     DVN data is ready to be transferred but does not actually do the transfer.  '''
     try:
-        
-        msg_json = {
+
+        message_json = {
             "dry_run": True,
             "application_name": "Dataverse",
             "package_id": "12345",
             "s3_path": s3_path,
             "s3_bucket_name": s3_bucket,
             "destination_path": destination_path,
-            "admin_metadata": {"original_queue": transfer_queue, "retry_count":0}
+            "admin_metadata": {"original_queue": transfer_queue, "retry_count": 0}
         }
 
         print("msg json:")
-        print(msg_json)
-        message = json.dumps(msg_json)
+        print(message_json)
+        message = json.dumps(message_json)
         connection_params = mqutils.get_transfer_mq_connection(transfer_queue)
-        #Default to one hour from now
-        now_in_ms = int(time.time())*1000
+        # Default to one hour from now
+        now_in_ms = int(time.time()) * 1000
         expiration = int(os.getenv('MESSAGE_EXPIRATION_MS', 36000000)) + now_in_ms
         print("Expiration: {}".format(expiration))
-        
-        connection_params.conn.send(transfer_queue, message, headers = {"persistent": "true", "expires": expiration})
+
+        connection_params.conn.send(transfer_queue, message, headers={"persistent": "true", "expires": expiration})
         print("MESSAGE TO QUEUE {} is {}".format(transfer_queue, message))
     except Exception as e:
         print(e)
-        raise(e)
-    return message
+        raise e
+    return message_json
+
 
 def notify_data_ready_transfer_message():
     '''Creates a dummy queue json message to notify the queue that the 
     DVN data is ready to be transferred but does not actually do the transfer.  '''
     try:
-        
-        msg_json = {
+
+        message_json = {
             "package_id": "doi-12-3456-transfer-service-test",
             "application_name": "Dataverse",
             "s3_path": s3_path,
             "s3_bucket_name": s3_bucket,
             "destination_path": destination_path,
-            "admin_metadata": {"original_queue": transfer_queue2, "retry_count":0}
+            "admin_metadata": {"original_queue": transfer_queue2, "retry_count": 0}
         }
 
         print("msg json:")
-        print(msg_json)
-        message = json.dumps(msg_json)
+        print(message_json)
+        message = json.dumps(message_json)
         connection_params = mqutils.get_transfer_mq_connection(transfer_queue2)
-        #Default to one hour from now
-        now_in_ms = int(time.time())*1000
+        # Default to one hour from now
+        now_in_ms = int(time.time()) * 1000
         expiration = int(os.getenv('MESSAGE_EXPIRATION_MS', 36000000)) + now_in_ms
         print("Expiration: {}".format(expiration))
-        
-        connection_params.conn.send(transfer_queue2, message, headers = {"persistent": "true", "expires": expiration})
+
+        connection_params.conn.send(transfer_queue2, message, headers={"persistent": "true", "expires": expiration})
         print("MESSAGE TO QUEUE {} is {}".format(transfer_queue2, message))
     except Exception as e:
         print(e)
-        raise(e)
+        raise e
     return message
-
-
-
--- tests/integration/test_mqutils.py
+++ tests/integration/test_mqutils.py
@@ -1,75 +1,71 @@
-import sys, os, logging, time, json, stomp
+import json
+import logging
+import sys
+import time
+from typing import Optional
+
+import stomp
+from mqresources import mqutils
+from stomp.utils import Frame
+
 sys.path.append('app/mqresources')
-import mqutils
-import mqlistener
 
 logging.basicConfig(format='%(message)s')
+
+transfer_queue = "/queue/dropbox-transfer-status-testing"
+
 
 def test_get_transfer_mq_connection():
     connection_params = mqutils.get_transfer_mq_connection()
     assert connection_params.conn is not None
 
+
 def test_notification():
     '''Sends a status message to the transfer queue and verifies that it made it'''
-    transfer_queue="/queue/dropbox-transfer-status-testing"
-    #Send the message
-    transfer_status = mqutils.TransferStatus("12345", "success", "/path/to/dropbox", {"original_queue" : transfer_queue, "retry_count":0})
-    message = mqutils.notify_transfer_status_message(transfer_status,transfer_queue)
+    # Send the message
+    transfer_status = mqutils.TransferStatus("12345", "success", "/path/to/dropbox",
+                                             {"original_queue": transfer_queue, "retry_count": 0})
+    message = mqutils.notify_transfer_status_message(transfer_status, transfer_queue)
     assert type(message) is str
-    messagedict = json.loads(message)
-    
-    connection_params = mqutils.get_transfer_mq_connection(transfer_queue)
-    mqlistenerobject = HelperMqListener(connection_params)
-      
-    conn = mqlistenerobject.get_connection()
-    conn.set_listener('', mqlistenerobject)
-    mqlistener.subscribe_to_listener(mqlistenerobject.connection_params)
-     
+    message_dict = json.loads(message)
+    mq_listener_object = TestConnectionListener()
+
     counter = 0
-    #Try for 30 seconds then fail
-    while mqlistenerobject.get_message_data() is None:
+    # Try for 30 seconds then fail
+    while mq_listener_object.get_message_body() is None:
         time.sleep(2)
-        counter = counter+2
-        if not conn.is_connected():
-            mqlistener.subscribe_to_listener(mqlistenerobject.connection_params)
+        counter = counter + 2
         if counter >= 10:
             assert False, "test_notification: could not find anything on the {} after 30 seconds".format(transfer_queue)
-    #dequeue the message
-    conn.ack(mqlistenerobject.get_message_id(), 1)    
-    assert mqlistenerobject.get_message_data() is not None
-    assert type(mqlistenerobject.get_message_data()) is dict
-    assert mqlistenerobject.get_message_data() == messagedict
-    
 
-class HelperMqListener(stomp.ConnectionListener):
-    '''A helper listener to consume the transfer status message placed on the queue'''
-    def __init__(self, connection_params):
-        self.connection_params = connection_params
-        self.message_data = None
-        self.message_id = None
-    def on_error(self, frame):
-        logging.debug('received an error "%s"' % frame.body)
-        print('received an error "%s"' % frame.body)
+    message_body = mq_listener_object.get_message_body()
+    assert message_body is not None
+    assert type(message_body) is dict
+    assert message_body == message_dict
 
-    def on_message(self, frame):
+    mq_listener_object.disconnect()
+
+
+class TestConnectionListener(stomp.ConnectionListener):
+    def __init__(self) -> None:
+        self._connection = self.__create_subscribed_connection()
+        self.message_body = None
+
+    def __create_subscribed_connection(self) -> stomp.Connection:
+        connection_params = mqutils.get_transfer_mq_connection(transfer_queue)
+        connection = connection_params.conn
+        connection_params.conn.set_listener('', self)
+        connection.subscribe(transfer_queue, id=1, ack="client-individual")
+        return connection
+
+    def on_message(self, frame: Frame) -> None:
         headers, body = frame.headers, frame.body
-        self.message_id = headers.get('message-id')
-        try: 
-            self.message_data = json.loads(body)
-        except json.decoder.JSONDecodeError: 
-            raise mqexception.MQException("Incorrect formatting of message detected.  Required JSON but received {} ".format(body))
-        
-        self.connection_params.conn.ack(self.message_id, 1)
+        message_id = headers.get('message-id')
+        self._connection.ack(message_id, 1)
+        self.message_body = json.loads(body)
 
-    def on_disconnected(self):
-        logging.debug('disconnected! reconnecting...')
-        subscribe_to_listener(self.connection_params)
-        
-    def get_connection(self):
-        return self.connection_params.conn
-    
-    def get_message_data(self):
-        return self.message_data
-    
-    def get_message_id(self):
-        return self.message_id
+    def get_message_body(self) -> Optional[dict]:
+        return self.message_body
+
+    def disconnect(self) -> None:
+        self._connection.disconnect()
